93c65b5d63d3dc87c7b0caa15e3e61e5cded72d1
76074@Rabbit <76074@Rabbit>
2025-02-08 15:04:56 +0800

    666

--- src/components/index.vue
+++ src/components/index.vue
@@ -70,6 +70,95 @@
                         </template>
                         <el-descriptions-item label="已上门数">999</el-descriptions-item>
                     </el-descriptions>
+                    <div style="text-align: center;margin: 10px auto;">客户资质表</div>
+                    <el-form style="max-width: 600px;margin: 0 auto;" ref="ruleFormRef" :rules="rules" :model="Form"
+                        label-width="auto" :label-position="labelPosition" :size="size">
+                        <el-form-item label="客户姓名" prop="name">
+                            <el-input v-model="Form.name" placeholder="请输入客户姓名" />
+                        </el-form-item>
+                        <el-form-item label="客户电话" prop="phone">
+                            <el-input v-model="Form.phone" placeholder="请输入客户的联系方式" />
+                        </el-form-item>
+                        <el-form-item label="客户邮箱">
+                            <el-input v-model="Form.email" placeholder="请输入客户的邮箱 有的话" />
+                        </el-form-item>
+                        <el-form-item label="加入时间">
+                            <el-col :span="11">
+                                <el-date-picker v-model="Form.date1" type="date" aria-label="Pick a date"
+                                    placeholder="Pick a date" style="width: 100%" />
+                            </el-col>
+                            <el-col class="text-center" :span="1" style="margin: 0 0.5rem">-</el-col>
+                            <el-col :span="11">
+                                <el-time-picker v-model="Form.date2" aria-label="Pick a time" placeholder="Pick a time"
+                                    style="width: 100%" />
+                            </el-col>
+                        </el-form-item>
+                        <el-form-item label="状态">
+                            <el-checkbox-group v-model="Form.status">
+                                <el-checkbox-button value="on" name="status">
+                                    已上门
+                                </el-checkbox-button>
+                                <el-checkbox-button value="off" name="status">
+                                    未上门
+                                </el-checkbox-button>
+                            </el-checkbox-group>
+                        </el-form-item>
+                        <el-form-item label="客户特征">
+                            <el-checkbox-group v-model="Form.trait">
+                                <el-checkbox label="0星：未核实到客户情况" value="" disabled />
+                                <br />
+                                <el-checkbox label="电话未接通" value="0_1" />
+                                <el-checkbox label="空号" value="0_2" />
+                                <el-checkbox label="停机" value="0_3" />
+                                <el-checkbox label="无法接通" value="0_4" />
+                                <el-checkbox label="通话中" value="0_5" />
+                                <el-checkbox label="用户忙" value="0_6" />
+
+                                <br />
+                                <el-checkbox label="1星：资质不符" value="" disabled />
+
+                                <br />
+                                <el-checkbox label="不在本地工作，没有可贷点" value="1_1" />
+                                <el-checkbox label="无任何资产" value="1_2" />
+
+                                <br />
+                                <el-checkbox label="2星：单一资质" value="" disabled />
+                                <br />
+                                <el-checkbox label="社保" value="2_1" />
+                                <el-checkbox label="车" value="2_2" />
+                                <el-checkbox label="外省按揭房" value="2_3" />
+                                <el-checkbox label="本地三人及以上共有房" value="2_4" />
+                                <el-checkbox label="保单" value="2_5" />
+
+                                <br />
+                                <el-checkbox label="2星+" value="" disabled />
+                                <br />
+                                <el-checkbox label="有住房公积金（公积金单边<600，双边<1200）" value="2_0_1" />
+
+                                <br />
+                                <el-checkbox label="3星" value="" disabled />
+                                <br />
+                                <el-checkbox label="有住房公积金（公积金单边≥600，双边≥1200）" value="3_1" />
+
+                                <br />
+                                <el-checkbox label="4星" value="" disabled />
+                                <br />
+                                <el-checkbox label="浙江省内按揭房（不包含杭州）（个人或夫妻共有，两人及以下共有" value="4_1" />
+                                <br />
+                                <el-checkbox label="5星" value="" disabled />
+                                <br />
+                                <el-checkbox label="杭州本地房（个人或夫妻共有，两人及以下共有房）（无论是全款，
+                                按揭，抵押）" value="5_1" />
+                            </el-checkbox-group>
+                        </el-form-item>
+                        <el-form-item>
+                            <el-button type="primary" @click="onSubmit">提交判断</el-button>
+                            <el-button @click="resetForm">取消</el-button>
+                        </el-form-item>
+                        <el-rate v-model="Form.star" disabled text-color="#ff9900" />
+                        <div style="color: #ff9900;">{{ Form.star }}星级客户！</div>
+                    </el-form>
+
                     <el-table :data="users" style="width: 50% ;margin-top: 50px;" :row-class-name="tableRowClassName"
                         height="250">
                         <el-table-column prop="ranking" label="排名" width="180" />
@@ -77,6 +166,33 @@
                         <el-table-column prop="get_m" label="已上门" />
                         <el-table-column prop="get_d" label="已签单" />
                     </el-table>
+                    <el-table :data="filterTableData" style="width: 100%">
+                        <el-table-column label="客户名" prop="name" />
+                        <el-table-column label="客户资质" prop="star" />
+                        <el-table-column label="添加日期" prop="date" />
+                        <el-table-column label="上门状态" prop="status" />
+                        <el-table-column align="right">
+                            <template #header>
+                                <el-input v-model="search" style="max-width: 600px" placeholder="查找客户"
+                                    class="input-with-select">
+                                    <template #prepend>
+                                        <el-button :icon="Search" />
+                                    </template>
+                                </el-input>
+                            </template>
+                            <template #default="scope">
+                                <el-button size="small" @click="onAddItem">
+                                    添加
+                                </el-button>
+                                <el-button size="small" type="danger" @click.prevent="deleteRow(scope.$index)">
+                                    删除
+                                </el-button>
+                            </template>
+                        </el-table-column>
+                    </el-table>
+                    <div style="text-align: center;width: 400px;">
+                        <VueDataUi component="VueUiDonut" :config="config" :dataset="dataset" />
+                    </div>
                     <div class="card">
                         <section class="landscape-section">
                             <div class="sky"></div>
@@ -211,7 +327,7 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed, reactive } from 'vue'
 import axios from "axios"
 import {
     Document,
@@ -221,6 +337,7 @@ import {
     Message
 } from '@element-plus/icons-vue';
 import dataCard from '../components/data_card.vue'
+import { Search } from '@element-plus/icons-vue'
 // 使用 ref 创建响应式数据
 const users = ref([]);
 
@@ -255,9 +372,350 @@ const addBreadcrumb = (label, path) => {
     breadcrumbs.value = [];
     breadcrumbs.value.push({ label, path }); // 添加新的面包屑项
 };
+
+const search = ref('')
+const tableData = ref([
+    {
+        date: '2016-05-03',
+        name: 'Tom',
+        star: '1星',
+        status: '已上门',
+    },
+    {
+        date: '2016-05-02',
+        name: 'John',
+        star: '2星',
+        status: '未上门',
+    },
+    {
+        date: '2016-05-04',
+        name: 'Morgan',
+        star: '3星',
+        status: '已上门',
+    },
+    {
+        date: '2016-05-01',
+        name: 'Jessy',
+        star: '4星',
+        status: '未上门',
+    }
+])
+
+const filterTableData = computed(() =>
+    tableData.value.filter(
+        (data) =>
+            !search.value ||
+            data.name.toLowerCase().includes(search.value.toLowerCase())
+    )
+)
+
+import dayjs from 'dayjs';
+const now = new Date(); // 当前日期
+const deleteRow = (index) => {
+    tableData.value.splice(index, 1);
+}
+
+const onAddItem = () => {
+    now.setDate(now.getDate() + 1);
+    tableData.value.push({
+        date: dayjs(now).format('YYYY-MM-DD'),
+        name: 'Tom',
+        star: '4星',
+        status: '未上门'
+    });
+}
+const size = ref('default'); // 默认大小
+const labelPosition = ref('left'); // 默认标签位置
+const ruleFormRef = ref();
+const Form = reactive({
+    name: '',
+    phone: '',
+    date1: '',
+    date2: '',
+    status: [],
+    trait: [],
+    star: 1
+});
+
+const rules = {
+    name: [
+        { required: true, message: '姓名为必填项', trigger: 'blur' },
+    ],
+    phone: [
+        { required: true, message: '手机号为必填项', trigger: 'blur' },
+        { pattern: /^[0-9]{11}$/, message: '手机号必须为11位数字', trigger: 'blur' },
+    ],
+    // 其他字段的校验规则...
+};
+
+const onSubmit = async () => {
+    const formEl = ruleFormRef.value;
+    if (!formEl) return;
+
+    await formEl.validate(async (valid, fields) => {
+        if (valid) {
+            console.log('提交的表单数据:', Form);
+            try {
+                // 发送 POST 请求，将表单数据存储到数据库
+                const response = await axios.post('http://localhost:3001/api/submit', Form);
+                console.log('提交成功:', response.data);
+            } catch (error) {
+                console.error('提交失败:', error);
+            }
+        } else {
+            console.log('表单校验失败:', fields);
+        }
+    });
+};
+
+const resetForm = () => {
+    Object.keys(Form).forEach(key => {
+        Form[key] = Array.isArray(Form[key]) ? [] : ''; // 重置数组为 []
+    });
+};
+import { VueDataUi } from "vue-data-ui";
+import "vue-data-ui/style.css"; // If you are using multiple components, place styles import in your main
+
+const config = ref({
+    type: 'polar',
+    responsive: false,
+    theme: '',
+    customPalette: [],
+    useCssAnimation: true,
+    useBlurOnHover: true,
+    userOptions: {
+        show: true,
+        showOnChartHover: false,
+        keepStateOnChartLeave: true,
+        position: 'right',
+        buttons: {
+            tooltip: true,
+            pdf: true,
+            csv: true,
+            img: true,
+            table: true,
+            labels: true,
+            fullscreen: true,
+            sort: false,
+            stack: false,
+            animation: false,
+            annotator: true
+        },
+        buttonTitles: {
+            open: 'Open options',
+            close: 'Close options',
+            tooltip: 'Toggle tooltip',
+            pdf: 'Download PDF',
+            csv: 'Download CSV',
+            img: 'Download PNG',
+            table: 'Toggle table',
+            labels: 'Toggle labels',
+            fullscreen: 'Toggle fullscreen',
+            annotator: 'Toggle annotator'
+        }
+    },
+    translations: {
+        total: 'Total',
+        average: 'Average'
+    },
+    table: {
+        show: false,
+        responsiveBreakpoint: 400,
+        th: {
+            backgroundColor: '#FFFFFFff',
+            color: '#1A1A1Aff',
+            outline: 'none'
+        },
+        td: {
+            backgroundColor: '#FFFFFFff',
+            color: '#1A1A1Aff',
+            outline: 'none',
+            roundingValue: 0,
+            roundingPercentage: 0
+        },
+        columnNames: {
+            series: 'Series',
+            value: 'Value',
+            percentage: 'Percentage'
+        }
+    },
+    style: {
+        fontFamily: 'inherit',
+        chart: {
+            useGradient: true,
+            gradientIntensity: '69',
+            backgroundColor: '#efebebff',
+            color: '#ccb3b3ff',
+            layout: {
+                labels: {
+                    dataLabels: {
+                        show: true,
+                        useLabelSlots: false,
+                        hideUnderValue: 3,
+                        prefix: '',
+                        suffix: ''
+                    },
+                    value: {
+                        rounding: 0,
+                        show: true,
+                        formatter: null
+                    },
+                    percentage: {
+                        color: '#56b3b2ff',
+                        bold: true,
+                        fontSize: 18,
+                        rounding: 0,
+                        formatter: null
+                    },
+                    name: {
+                        color: '#3a3131ff',
+                        bold: true,
+                        fontSize: 14
+                    },
+                    hollow: {
+                        show: true,
+                        total: {
+                            show: true,
+                            bold: false,
+                            fontSize: 18,
+                            color: '#AAAAAAff',
+                            text: 'Total',
+                            offsetY: 0,
+                            value: {
+                                color: '#1A1A1Aff',
+                                fontSize: 18,
+                                bold: true,
+                                suffix: '',
+                                prefix: '',
+                                offsetY: 0,
+                                rounding: 0,
+                                formatter: null
+                            }
+                        },
+                        average: {
+                            show: true,
+                            bold: false,
+                            fontSize: 18,
+                            color: '#AAAAAAff',
+                            text: 'Average',
+                            offsetY: 0,
+                            value: {
+                                color: '#9f9999ff',
+                                fontSize: 18,
+                                bold: true,
+                                suffix: '',
+                                prefix: '',
+                                offsetY: 0,
+                                rounding: 0,
+                                formatter: null
+                            }
+                        }
+                    }
+                },
+                donut: {
+                    strokeWidth: 55,
+                    borderWidth: 1,
+                    useShadow: false,
+                    shadowColor: '#1A1A1A'
+                }
+            },
+            comments: {
+                show: true,
+                showInTooltip: true,
+                width: 100,
+                offsetY: 0,
+                offsetX: 0
+            },
+            legend: {
+                show: true,
+                bold: false,
+                backgroundColor: '#FFFFFFff',
+                color: '#847575ff',
+                fontSize: 16,
+                roundingValue: 0,
+                roundingPercentage: 0
+            },
+            tooltip: {
+                show: true,
+                color: '#1A1A1Aff',
+                backgroundColor: '#FFFFFFff',
+                fontSize: 14,
+                customFormat: null,
+                borderRadius: 4,
+                borderColor: '#e1e5e8',
+                borderWidth: 1,
+                backgroundOpacity: 30,
+                position: 'center',
+                offsetY: 24,
+                showValue: true,
+                showPercentage: true,
+                roundingValue: 0,
+                roundingPercentage: 0
+            },
+            title: {
+                text: '重要程度',
+                color: '#1A1A1Aff',
+                fontSize: 20,
+                bold: true,
+                textAlign: 'center',
+                paddingLeft: 0,
+                paddingRight: 0,
+                subtitle: {
+                    color: '#A1A1A1ff',
+                    text: '',
+                    fontSize: 16,
+                    bold: false
+                }
+            }
+        }
+    }
+});
+
+const dataset = ref([
+    {
+        name: '一星级客户',
+        values: [
+            25
+        ],
+        color: '#deedf7'
+    },
+    {
+        name: '二星级客户',
+        values: [
+            25
+        ],
+        color: '#d7ca89'
+    },
+    {
+        name: '三星级客户',
+        values: [
+            25
+        ],
+        color: '#0fff77'
+    },
+    {
+        name: '四星级客户',
+        values: [
+            25
+        ],
+        color: '#6fbcec'
+    },
+    {
+        name: '五星级客户',
+        values: [
+            25
+        ],
+        color: '#e83521'
+    }
+]);
+
 </script>
 
 <style lang="scss" scoped>
+.el-radio-group {
+    margin-right: 12px;
+}
+
 .aside {
     height: 60px;
     line-height: 60px;
